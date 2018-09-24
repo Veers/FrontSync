61e72de25480db5c81d967111d7eaf6db5d7a943
Veers <a.konviser@gmail.com>
2018-09-24 17:36:06 +0300

    Add router (in progress)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import FormContainer from "./js/components/container/FormContainer";
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import {App} from './components/App';
+
+render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
---
+++ src/js/App.js
@@ -0,0 +1,9 @@
+import FormContainer from "./js/components/container/FormContainer";
+
+const App = () => (
+  <div>
+    <FormContainer />
+  </div>
+)
+
+export default App
